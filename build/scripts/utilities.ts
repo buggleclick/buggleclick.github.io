508b659747cbf7933738327b5b0f1a29852d8c23
HazardWorks <domelement0001@gmail.com>
2019-09-16 10:15:37 -0400

    Rebuilding "utilities.ts" for larger numbers

--- build/scripts/utilities.ts
+++ build/scripts/utilities.ts
@@ -1,32 +1,12 @@
-const numberWritten: string[] = [
-    '',' Million',' Billion',' Trillion',' Quadrillion',' Quintillion',
-    ' Sextillion',' Septillion',' Octillion',' Nonillion',' Decillion'
+const formatLong: string[] = [
+    ' thousand',' million',' billion',' trillion',' quadrillion',
+    ' quintillion',' sextillion',' septillion',' octillion',' nonillion'
 ];
-
-const appendCommas = function(value: string): string {
-    return value.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-}
-
-const parseLargeNumber = function(value: number): string {
-    if(value <= 999999) {
-        return appendCommas(value.toString());
-    } else {
-        let baseVal: number = 0;
-        let notation: string = String();
-        if(!isFinite(value)){ 
-            return 'Uncalculable!';
-        } else {
-            let thisVal: number = (value /= 1000);
-            while(Math.round(value) >= 1000) {
-                thisVal /= 1000;
-                baseVal++;
-            }
-            if(baseVal >= numberWritten.length) {
-                return 'Uncalculable!';
-            } else {
-                notation = numberWritten[baseVal];
-            }
-            return (Math.round(thisVal * 1000) / 1000) + notation;
-        }
-    }
-}
+const prefixesLong: string[] = [
+    '','un','duo','tre','quattuor','quin','sex','septen','octo','novem'
+];
+const suffixLong: string[] = [
+    'decillion','vigintillion','trigintillion','quadragintillion',
+    'quinquagintillion','sexagintillion','septuagintillion','octogintillion',
+    'nonagintillion'
+]
