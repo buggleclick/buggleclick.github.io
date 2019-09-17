5cfcb705c2f26d45060747af88ced56bc23bfc57
HazardWorks <domelement0001@gmail.com>
2019-09-17 11:26:38 -0400

    Fix of "CHANGELOG.md" and "renderChangelog.ts"

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,2 @@
-#### v0.0.0-alpha
+#### Version 0.0.0-alpha
 - First build of file
--- build/scripts/renderChangelog.ts
+++ build/scripts/renderChangelog.ts
@@ -11,3 +11,6 @@ function renderChangelog(): void {
         })
     })
 }
+
+// Actually render the changelog!
+renderChangelog();
