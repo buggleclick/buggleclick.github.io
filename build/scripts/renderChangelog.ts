08e7c70aa88b86ce0ca1d5364c29fab94f724295
HazardWorks <domelement0001@gmail.com>
2019-09-16 20:23:53 -0400

    Add "renderChangelog.ts" & finish "changelog.pug"

---
+++ build/scripts/renderChangelog.ts
@@ -0,0 +1,13 @@
+function element(elem: string): HTMLElement | null {
+    return document.querySelector(elem);
+}
+
+function renderChangelog(): void {
+    CHANGELOG.forEach((change: { version: string; changes: string[]; }) => {
+        element('#changelog').innerHTML += `<h3 class="text-primary">${change.version}</h3>`;
+        element('#changelog').innerHTML += `<ul>`;
+        change.changes.forEach((text: string) => {
+            element('#changelog').innerHTML += `<li>${text}</li>`;
+        })
+    })
+}
